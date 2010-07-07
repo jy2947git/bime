66c0dab0614b3730f9b6f5218127c75e68e77619
jy2947git <jy2947@yahoo.com>
2010-07-07 13:51:12 -0500

    git

--- scripts/automation/build.sh
+++ scripts/automation/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
-# this script will download the source code from SVN server and run the Ant build script to generate the distribution zip file
+# this script will download the source code and run the Ant build script to generate the distribution zip file
 # make sure Ant is already installed "sudo apt-get install ant"
-# make sure SVN is installed "sudo apt-get install subversion"
+# make sure Git is installed "sudo apt-get install git"
+# 
+# it is assuming the bime directory already exists
+# One usage of this script is to build latest code on Bime instance
 #
-#
+
+cd /usr/local/bime-home/bime
 if [ -d source ]; then
 echo 'find source directory'
 else
@@ -12,8 +16,8 @@ echo 'creating source directory'
 mkdir source
 fi
 cd source
-echo 'download file from svn....'
-svn --username jy2947 --password jy2947 --non-interactive checkout --force http://svn.bimelab.com/svn/svn-repository/bime
+echo 'download file from github....'
+git clone git@github.com:jy2947git/bime.git
 echo 'running build script'
 ant -f bime/build.xml
 cp /usr/local/bime-home/bime/source/bime/dist/bime/bime.zip /usr/local/bime-home/bime
